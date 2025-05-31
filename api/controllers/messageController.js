5ab7b85d8d6d1d9c5db910084a96b5faa9457b87
shourya-r <rajputshourya7@gmail.com>
2025-05-31 22:22:48 +0530

    Add message model, routes and controllers

---
+++ api/controllers/messageController.js
@@ -0,0 +1,48 @@
+import Message from "../models/MessageModel.js";
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { content, receiverId } = req.body;
+
+    const newMessage = await Message.create({
+      sender: req.user.id,
+      receiver: receiverId,
+      content,
+    });
+
+    //TODO: SEND THE MESSAGE IN REAL TIME => SOCKET.IO
+    res.status(201).json({
+      success: true,
+      message: newMessage,
+    });
+  } catch (error) {
+    console.log("Error in sendMessage: ", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+export const getConversation = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const messages = await Message.find({
+      $or: [
+        { sender: req.user._id, receiver: userId },
+        { sender: userId, receiver: req.user._id },
+      ],
+    }).sort("createdAt");
+
+    res.status(200).json({
+      success: true,
+      messages,
+    });
+  } catch (error) {
+    console.log("Error in getConversation: ", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
