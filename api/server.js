b1dc9e35512458e13e81bd49a727baca7a9b3ce0
shourya-r <rajputshourya7@gmail.com>
2025-06-22 13:11:54 +0530

    Updated URL

--- api/server.js
+++ api/server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true, // Allow cookies to be sent with requests
   })
 );
