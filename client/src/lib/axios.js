c57388c5a1cbb22c256c0971374623e90e5bbeee
shourya-r <rajputshourya7@gmail.com>
2025-06-22 12:56:39 +0530

    Updated variables for deployment

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-// TODO: update the baseURL so that it works in production
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: BASE_URL,
   withCredentials: true, // send cookies with requests
 });
--- client/src/socket/socket.client.js
+++ client/src/socket/socket.client.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:5000";
+const SOCKET_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/";
 
 let socket = null;
 
